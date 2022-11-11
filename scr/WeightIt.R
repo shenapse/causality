9e138852692fe7e99918fd6e5e75f9094138236d
shena <87386937+Shena4746@users.noreply.github.com>
2022-11-11 18:43:34 +0900

    matching and estimate ATT by WeightIt package

---
+++ scr/WeightIt.R
@@ -0,0 +1,46 @@
+library(magrittr)
+
+path_data <- project_root$find_file("data/ihdp_obs.csv")
+df <- read.table(path_data, header = TRUE)
+
+df_juice <- recipes::recipe(df, treatment ~ .) %>%
+    # add interaction term for reduce in-balance (see balance check below)
+    # recipes::step_interact(terms = ~ X6:X4) %>%
+    recipes::step_interact(terms = ~ -y_factual) %>%
+    recipes::step_normalize(-where(is.integer), -y_factual) %>%
+    recipes::prep() %>%
+    recipes::juice()
+
+# propensity score matching by weightit
+estimand <- "ATT"
+# W.pscore.out <- WeightIt::weightit(treatment ~ . - y_factual, data = df_juice, estimand = estimand, method = "ps")
+# see how the weights are estimated
+# W.pscore.out %>% print()
+# see summary stat of the weighting
+# W.pscore.out %>% summary()
+# entropy balancing by weigthit
+W.entropy.out <- WeightIt::weightit(treatment ~ . - y_factual, data = df_juice, estimand = estimand, method = "ebal")
+# W.entropy.out %>% summary()
+
+# balancing check by cobalt
+cobalt::set.cobalt.options(imbalanced.only = TRUE)
+# on propensity score version
+# cobalt::bal.tab(W.pscore.out, stats = c("m", "v"), thresholds = c(m = .05), imbalanced.only = TRUE)
+# on entropy weighting
+cobalt::bal.tab(W.entropy.out, stats = c("m", "v"), thresholds = c(m = .05))
+
+# by boot-strapping
+
+# outcome model
+outcome_model_trained <- lm(y_factual ~ ., data = df_juice, weights = W.entropy.out$weights)
+coef(outcome_model_trained)["treatment"]
+
+# boot-stropping std
+est.fn <- function(data, index) {
+    w.out <- WeightIt::weightit(treatment ~ . - y_factual, data = data[index, ], estimand = estimand, method = "ebal")
+    model_trained <- lm(y_factual ~ ., data = data[index, ], weights = w.out$weights)
+    return(coef(model_trained)["treatment"])
+}
+
+boot.out <- boot::boot(statistic = est.fn, data = df_juice, R = 2000)
+boot::boot.ci(boot.out, type = "bca")
